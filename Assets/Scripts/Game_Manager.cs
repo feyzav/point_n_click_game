f6358703a444ae313f2a10f0707de38c86ff3400
Feyza Vidinli <feyzavdnl@hotmail.com>
2020-12-09 19:29:33 +0300

    Cube's collider issue fixed
    Cube's collider wasn't working properly . Collider has fixed.

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -19,6 +19,7 @@ public class Game_Manager : MonoBehaviour
         if (Input.GetMouseButtonDown(1) && currentNode.GetComponent<Prop>() != null)
         {
             currentNode.GetComponent<Prop>().loc.Arrive();
+            print("clicked");
         }
     }
 }
